349b75f3f8396d1f9ed89bc585fe604b0383a907
DunklerK3ks <83074092+DunklerK3ks@users.noreply.github.com>
2021-04-26 22:03:41 +0200

    Create Rhnus-Nr1.py

---
+++ Rhnus-Nr1.py
@@ -0,0 +1,4 @@
+# Made by Tim Mathis // DunklerKeks
+for zahl in range(1,100): # Aufzählung
+    if zahl % 3 == 0 or zahl % 5 == 0:# Überprüfung der Teilbarkeit
+        print(zahl , "Ist Teilbar")# Die Print ausgabe das es Teilbar ist

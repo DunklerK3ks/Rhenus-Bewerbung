113c099e094b5963df8717f0325b98322f297244
DunklerK3ks <83074092+DunklerK3ks@users.noreply.github.com>
2021-04-27 23:56:04 +0200

    Add files via upload

---
+++ Rhenus -Nr 1.py
@@ -0,0 +1,6 @@
+# Made by Tim Mathis // DunklerKeks
+for zahl in range(1,100): # Aufzählung
+    if zahl % 3 == 0 :# Überprüfung der Teilbarkeit
+        print(zahl , "Ist durch 3 teilbar")# Die Print ausgabe das es Teilbar ist
+    if zahl % 5 == 0 :# Überprüfung der Teilbarkeit
+        print(zahl , "Ist durch 5 teilbar")# Die Print ausgabe das es Teilbar ist
